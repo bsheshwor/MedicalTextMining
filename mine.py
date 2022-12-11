218670c8f09fcf9cb98bdcfba1878af12ccbd530
Bisheshwor Neupane <bn02433818@student.ku.edu.np>
2022-12-11 15:07:42 +0545

    unique

--- mine.py
+++ mine.py
@@ -6,26 +6,33 @@ from wordcloud import WordCloud
 import CONFIG
 from data_management import text_2df
 
+def unique(arr_list):
+  new_list = []
+
+  for one_arr_choice in arr_list:
+      if one_arr_choice not in new_list:
+          new_list.append(one_arr_choice)
+  return new_list
 
 def diseaseExtract(text):
   dataofx = CONFIG.nlp_obj(text)
   results = [ent.text for ent in dataofx.ents if ent.label_ == 'DISEASE']
-  return results
+  return unique(results)
 
 def chemicalExtract(text):
   dataofx = CONFIG.nlp_obj(text)
   results = [ent.text for ent in dataofx.ents if ent.label_ == 'CHEMICAL']
-  return results
+  return unique(results)
 
 def organExtract(text):
   dataofxobj2 = CONFIG.nlp_obj2(text)
   results = [ent.text for ent in dataofxobj2.ents if ent.label_ == 'ORGAN']
-  return results
+  return unique(results)
 
 def get_common_disease(df,x):
   disease_list = df['Disease'].tolist()
   disease_list = [i for j in disease_list for i in j]
-  return disease_list
+  return unique(disease_list)
 
 def plot_disease_cloud(text):
   mywordcloud = WordCloud().generate(text)
@@ -36,24 +43,15 @@ def plot_disease_cloud(text):
 def get_common_chemicals(df,x):
   chemical_list = df['Chemicals'].tolist()
   chemical_list = [i for j in chemical_list for i in j]
-  return chemical_list
+  return unique(chemical_list)
 
 
 def spacy_mine():
     df = text_2df()
-    # for i in range(df.shape[0]):
-    #     x = df['Text'][i]
-    #     dataofx= CONFIG.nlp_obj(x)
-    #     df.loc[i,['Disease','Chemicals']] = [diseaseExtract(x),chemicalExtract(x)]
-    
     df['Disease'] = df['Text'].apply(lambda x:diseaseExtract(x))
     df['Chemicals'] = df['Text'].apply(lambda x:chemicalExtract(x))
-
     surgery_disease = get_common_disease(df,'Surgery')
     chemicals = get_common_chemicals(df,'Chemicals')
-
     df['Organ'] = df['Text'].apply(lambda x:organExtract(x))
-
-
     return df
 
